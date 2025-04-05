8c67d68e3c42bb08717d5c6d20b3c940faf145ce
lan <lan@aladinfun.com>
2025-04-05 18:34:24 +0800

    README

--- run.go
+++ run.go
@@ -1,11 +1,20 @@
 package traffic
 
 import (
+	"fmt"
 	"github.com/zhanmengao/gateway/global"
 	"github.com/zhanmengao/gateway/internal/ts"
+	"net/http"
 )
 
 func Run(port int64, rt global.IRoute, urlList []*global.DBUrl) (err error) {
 	ts.SetURLList(urlList)
+	global.Route = rt
+
+	hSrv := http.NewServeMux()
+	hSrv.Handle("/", &ts.HttpTrafficServer{})
+	if err = http.ListenAndServe(fmt.Sprintf(":%d", port), hSrv); err != nil {
+		panic(err)
+	}
 	select {}
 }
